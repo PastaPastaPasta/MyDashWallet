9cc865d40d06dbe247fa193717fa189848855c32
PastaPastaPasta <6443210+PastaPastaPasta@users.noreply.github.com>
2024-03-21 17:43:57 +0000

    Deploying to gh-pages from @ PastaPastaPasta/MyDashWallet@0209819a3160daabdff5b9cf4795401b513d0ddf 🚀

---
+++ precache-manifest.d6f9c443ae989a1062b14e3f7fa4fffa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a45e8f6fac0d822d9bf25b7d3285339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0e33085f0951fd3af2f",
+    "url": "/static/css/2.a3e1401a.chunk.css"
+  },
+  {
+    "revision": "00af3e7f0f648da5e8f1",
+    "url": "/static/css/main.4f68c770.chunk.css"
+  },
+  {
+    "revision": "d0e33085f0951fd3af2f",
+    "url": "/static/js/2.505a2b67.chunk.js"
+  },
+  {
+    "revision": "a55cbc881fdde147a95b9ca2fa6b9bda",
+    "url": "/static/js/2.505a2b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00af3e7f0f648da5e8f1",
+    "url": "/static/js/main.68f35036.chunk.js"
+  },
+  {
+    "revision": "9fe0d3c75af36d14d387",
+    "url": "/static/js/runtime-main.786c1fbd.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
